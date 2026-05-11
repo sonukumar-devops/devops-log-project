23b37f3df4184a81c60301e57b6c4091c57f012d
Sonu Kumar <2014sonukr2015@gmail.com>
2026-05-11 23:53:33 +0530

    Add pipeline2 with updated exit status

--- analyzer.py
+++ analyzer.py
@@ -1,30 +1,44 @@
 import time
 import os
+import sys
 
 log_file = "app.log"
 
 print("Analyzer started...", flush=True)
 
 try:
+    timeout = 10   # seconds
+    start_time = time.time()
+
     while True:
+        # ⏱️ Stop after timeout
+        if time.time() - start_time > timeout:
+            print("No ERROR found. Exiting cleanly.", flush=True)
+            sys.exit(0)
+
         if not os.path.exists(log_file):
             print("Waiting for log file...", flush=True)
-            time.sleep(2)
+            time.sleep(1)
             continue
 
         with open(log_file, "r") as f:
             print("Reading logs...", flush=True)
-
-            f.seek(0, os.SEEK_END)   # go to end
+            f.seek(0, os.SEEK_END)
 
             while True:
+                if time.time() - start_time > timeout:
+                    print("No ERROR found. Exiting cleanly.", flush=True)
+                    sys.exit(0)
+
                 line = f.readline()
 
                 if line:
                     if "ERROR" in line:
                         print(f"ALERT: {line.strip()}", flush=True)
+                        sys.exit(1)   # ❌ FAIL
                 else:
                     time.sleep(1)
 
 except Exception as e:
     print(f"Some error occurred: {e}", flush=True)
+    sys.exit(1)
